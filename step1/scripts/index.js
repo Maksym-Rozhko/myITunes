cd20dcb20a03f7bb9db32d266cc29376fa2d0050
Maksym <maksymRozhko7@gmail.com>
2020-07-28 01:47:10 +0300

    first commit

---
+++ step1/scripts/index.js
@@ -0,0 +1,30 @@
+import { radioPlayerInit } from './radioPlayer.js';
+import { musicPlayerInit } from './musicPlayer.js';
+import { videoPlayerInit } from './videoPlayer.js';
+
+const playerBtns = document.querySelectorAll('.player-btn');
+const playerBlocks = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
+
+
+const deactivationBtn = () => {
+    temp.style.display = 'none';
+    playerBtns.forEach(item => item.classList.remove('active'));
+    playerBlocks.forEach(item => item.classList.remove('active'));
+};
+
+
+
+playerBtns.forEach((btns, i) => btns.addEventListener('click', () => {
+    deactivationBtn();
+    btns.classList.add('active');
+    playerBlocks[i].classList.add('active');
+}));
+
+
+
+
+
+videoPlayerInit();
+musicPlayerInit();
+radioPlayerInit();
