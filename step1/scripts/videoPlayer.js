33c763201adf82b55002a2644ecb40e3bea93ef4
Maksym <maksymRozhko7@gmail.com>
2020-07-30 21:54:41 +0300

    Wrote a script for a audio player
    Added functionality to the audio player

--- step1/scripts/videoPlayer.js
+++ step1/scripts/videoPlayer.js
@@ -1,3 +1,5 @@
+import { addZero } from './support.script.js';
+
 export const videoPlayerInit = () => {
 
     const videoPlayer = document.querySelector('.video-player');
@@ -18,7 +20,7 @@ export const videoPlayerInit = () => {
             videoButtonPlay.classList.remove('fa-play');
         }
     }
-
+    
     const togglePlay = () => {
         if (videoPlayer.paused) {
             videoPlayer.play();
@@ -31,8 +33,6 @@ export const videoPlayerInit = () => {
         videoPlayer.pause();
         videoPlayer.currentTime = 0;
     };
-
-    const addZero = n => n < 10 ? '0' + n : n;
 
     videoPlayer.addEventListener('click', togglePlay);
     videoButtonPlay.addEventListener('click', togglePlay);
